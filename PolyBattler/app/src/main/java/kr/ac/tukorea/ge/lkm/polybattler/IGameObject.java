da50c0a37bef46cf2d4d201541ee82933ece02c8
LAPTOP-LKM2025\rhksa <rhksals7864@gmail.com>
2025-04-08 12:25:11 +0900

    make project android studio

---
+++ PolyBattler/app/src/main/java/kr/ac/tukorea/ge/lkm/polybattler/IGameObject.java
@@ -0,0 +1,8 @@
+package kr.ac.tukorea.ge.lkm.polybattler;
+
+import android.graphics.Canvas;
+
+public interface IGameObject {
+    public void update();
+    public void draw(Canvas canvas);
+}
---
+++ PolyBattler/app/src/main/java/kr/ac/tukorea/ge/lkm/polybattler/Metrics.java
@@ -0,0 +1,6 @@
+package kr.ac.tukorea.ge.lkm.polybattler;
+
+public class Metrics {
+    public static final float SCREEN_WIDTH = 9.0f;
+    public static final float SCREEN_HEIGHT = 16.0f;
+}
